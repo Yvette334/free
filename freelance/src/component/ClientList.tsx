2405e83020d57698252242740ac5fd9bb96cc694
Yvette334 <y.muhoracye@alustudent.com>
2025-11-02 16:24:26 +0200

    Added the client list

--- freelance/src/component/ClientList.tsx
+++ freelance/src/component/ClientList.tsx
@@ -1,12 +1,9 @@
 import type { Client } from "../types/app"
-interface Props {
-  client: Client
-}
 
-export default function ClientList({ client }: Props) {
+export default function ClientList({ client }: { client: Client }) {
   return (
-    <div className="border rounded-lg p-4 bg-gray-800 text-white">
-      <h2 className="font-bold text-lg">{client.name}</h2>
+    <div className="border rounded p-3 bg-gray-800 text-white">
+      <h3 className="font-bold">{client.name}</h3>
       <p>{client.country}</p>
       {client.email && <p className="text-sm text-gray-400">{client.email}</p>}
     </div>
