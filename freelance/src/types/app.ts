80a0b3dc05b8aeea9d6b4cb852ad909c6bd7f922
Yvette334 <y.muhoracye@alustudent.com>
2025-11-02 10:35:45 +0200

    created the interfaces

---
+++ freelance/src/types/app.ts
@@ -0,0 +1,24 @@
+export interface Client {
+    id: string
+    name: string
+    country: string
+}
+
+export interface Product{
+    id: string
+    clientId: string
+    title: string
+    budget: number
+    status: ProjectStatus;
+    paymentStatus: PaymentStatus;
+    
+}
+
+export interface Payment{
+    projectId: string
+    amount: number
+    date: string
+}
+
+export type ProjectStatus = "pending" | "in-progress" | "completed";
+export type PaymentStatus = "paid" | "unpaid";
